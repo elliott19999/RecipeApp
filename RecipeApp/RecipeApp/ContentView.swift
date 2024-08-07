1098d13cb58ba430d4a503a6f127f87622cf1649
elliott <elliott>
2024-08-07 14:18:29 +0300

    no message

---
+++ RecipeApp/RecipeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeApp
+//
+//  Created by Мадина Валиева on 07.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+      
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeApp/RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Мадина Валиева on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
